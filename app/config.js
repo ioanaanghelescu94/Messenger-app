cf29f7d1fb3d87997a5e70e50f03cc6ba1240c6c
ioanaanghelescu94 <ioana.anghelescu94@gmail.com>
2025-05-24 00:08:12 +0300

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 const config =  {
-	PORT: 4000,
-	DB_ADDRESS: "mongodb://localhost:27017/messenger",
-	JWT_SECRET_KEY: "1q7PmGH04phLl5k6c2AisEda2y286UtKPxwrCz3T1M",
+	PORT: placeholder,
+	DB_ADDRESS: "placeholder",
+	JWT_SECRET_KEY: "placeholder",
 	JWT_EXPIRE_TIME: 60 * 60 * 24
 }
 
